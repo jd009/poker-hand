5d493d3500b9b941fe99e24794cdda6d1a6f29e0
John D <deblanc.john@gmail.com>
2016-03-06 11:41:47 -0600

    Adding more PokerHandRecord tests to test saving and retrieving info about a poker hand. Also, moving shared setup into beforeEach.

--- test/pokerHandRecord_test.js
+++ test/pokerHandRecord_test.js
@@ -1,9 +1,15 @@
 var assert = require('assert');
 var PokerHandRecord = require('../pokerHandRecord.js');
+var Card = require('../card.js');
 
 describe('PokerHandRecord', function() {
+  var pokerHandRecord = null;
+
+  beforeEach(function() {
+    pokerHandRecord = new PokerHandRecord();
+  });
+
   it('should properly determine the right storage index for a face card rank', function() {
-    var pokerHandRecord = new PokerHandRecord();
     var kingRank = 'K';
     var expectedKingIndex = 13;
     var kingIndex = pokerHandRecord._getStorageIndex(kingRank);
@@ -11,7 +17,6 @@ describe('PokerHandRecord', function() {
   });
 
   it('should properly determine the right storage index for a number card rank', function() {
-    var pokerHandRecord = new PokerHandRecord();
     var tenRank = '10';
     var expectedTenIndex = 10;
     var tenIndex = pokerHandRecord._getStorageIndex(tenRank);
@@ -19,7 +24,6 @@ describe('PokerHandRecord', function() {
   });
 
   it('should throw an exception for an invalid high number card rank', function() {
-    var pokerHandRecord = new PokerHandRecord();
     var invalidTooHighRank = '11';
     assert.throws(function() {
       var storageIndex = pokerHandRecord._getStorageIndex(invalidTooHighRank);
@@ -27,10 +31,42 @@ describe('PokerHandRecord', function() {
   });
 
   it('should throw an exception for an invalid low number card rank', function() {
-    var pokerHandRecord = new PokerHandRecord();
     var invalidTooLowRank = '1';
     assert.throws(function() {
       var storageIndex = pokerHandRecord._getStorageIndex(invalidTooLowRank);
     });
+  });
+
+  it('should save a card by suit and rank', function() {
+    var aceRank = 'A';
+    var spadesSuit = 'S';
+    var testAceOfSpadesCard = new Card(aceRank, spadesSuit);
+    pokerHandRecord.saveCard(testAceOfSpadesCard);
+    var storageIndex = pokerHandRecord._getStorageIndex(aceRank);
+    assert.equal(pokerHandRecord.rankRecordStorage[storageIndex][0], testAceOfSpadesCard);
+    assert.equal(pokerHandRecord.suitRecordStorage[spadesSuit][0], testAceOfSpadesCard);
+  });
+
+  it('should return the correct number of suits in a hand', function() {
+    var testTwoOfHeartsCard = new Card('2', 'H');
+    pokerHandRecord.saveCard(testTwoOfHeartsCard);
+    var testThreeOfClubsCard = new Card('3', 'C');
+    pokerHandRecord.saveCard(testThreeOfClubsCard);
+
+    var expectedNumSuits = 2;
+    var numSuits = pokerHandRecord.getNumSuits();
+    assert.equal(numSuits, expectedNumSuits);
+  });
+
+  it('should return the correct number of cards at a rank', function() {
+    var sharedRank = '2';
+    var testTwoOfHeartsCard = new Card(sharedRank, 'H');
+    pokerHandRecord.saveCard(testTwoOfHeartsCard);
+    var testTwofClubsCard = new Card(sharedRank, 'C');
+    pokerHandRecord.saveCard(testTwofClubsCard);
+
+    var expectedNumOfCardsAtRank = 2;
+    var numCardsAtRank = pokerHandRecord.getNumCards(sharedRank);
+    assert.equal(numCardsAtRank, expectedNumOfCardsAtRank);
   });
 });
