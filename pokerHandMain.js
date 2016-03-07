f8d990ca903baa5eff2781315b243f7ac10696ce
John D <deblanc.john@gmail.com>
2016-03-07 01:16:23 -0600

    Test for pokerHandScore

--- pokerHandMain.js
+++ pokerHandMain.js
@@ -6,8 +6,8 @@ var pokerHandRanker = new PokerHandRanker();
 
 var testStringArray = [];
 testStringArray.push('AC AD 7H 5D 5H');
-testStringArray.push('AC AD 7H 5D 6C');
-testStringArray.push('AC KH QD JD 10C');
+testStringArray.push('AH AD 7H 5D 6C');
+testStringArray.push('AS KH QD JD 10C');
 testStringArray.push('2C 2D 2H JH 9C');
 testStringArray.push('3C 4D 4H 4C 4S');
 testStringArray.push('QH JH KS AC 10C');
---
+++ test/pokerHandScore_test.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var PokerHandScore = require('../helpers/pokerHandScore.js');
+var Card = require('../helpers/card.js');
+
+describe('PokerHandScore', function() {
+  it('should report only high card if no other ranking', function() {
+    var pokerHandScore = new PokerHandScore();
+    pokerHandScore.highCard = new Card('A', 'S');
+
+    var reportArray = pokerHandScore.printToArray();
+    assert.equal(reportArray.length, 1);
+  });
+
+  it('should have two report elements when ranking beyond high card', function() {
+    var pokerHandScore = new PokerHandScore();
+    pokerHandScore.highCard = new Card('A', 'S');
+    var pair = [];
+    pair.push(new Card('7', 'C'));
+    pair.push(new Card('7', 'D'));
+    pokerHandScore.arrayOfPairs.push(pair);
+
+    var reportArray = pokerHandScore.printToArray();
+    assert.equal(reportArray.length, 2);
+  });
+});
